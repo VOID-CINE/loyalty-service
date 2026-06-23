4417e88ea3e551b59f560b7dc5a7304b32bb67ca
Darko <dark.araya@duocuc.cl>
2026-06-22 20:56:55 -0400

    se borran carpetas malas

--- user-service/user-service/src/main/java/com/cine/user_service/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cine.user_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cine.user_service.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    
-}
